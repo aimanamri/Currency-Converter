fd326c533eeada01371ddc106ea3e5bf4b68f43d
Aiman Amri <aimanuddin@gmail.com>
2021-06-04 05:09:27 +0900

    Fix side panel for 課程/Program selection box

--- syllabusapp.py
+++ syllabusapp.py
@@ -6,24 +6,34 @@ import re
 
 st.set_page_config(layout="wide")
 
-url1 = 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=040&page=' #kikai
-url2 = 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=037&page=' #jouhou
-url3 = 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=036&page=' #denki
-url4 = 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=039&page=' #dezain keiei
+URL = [
+['機械工学課程' , 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=040&page='],
+['情報工学課程' , 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=037&page='],
+['電子システム工学課程' , 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=036&page='],
+['デザイン経営工学課程' , 'https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=05&sc=039&page='],
+['デザイン・建築学課程','https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=06&sc=041&page='],
+['応用化学課程 ','https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=08&sc=032&page='],
+['応用生物学課程','https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=04&sc=032&page='],
+['高分子機能工学課程','https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=04&sc=034&page='],
+['物質工学課程','https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=04&sc=035&page='],
+['留学生科目','https://www.syllabus.kit.ac.jp/?c=search_list&sk=00&dc=01&ac=11&page='] 
+]
+
+# # Create the pandas DataFrame for url links
+df = pd.DataFrame(URL, columns=['URL', 'link'])
+values = df['URL'].tolist()
+options = df['link'].tolist()
+dic = dict(zip(options, values))
+
+# Side Panel
+url = st.sidebar.selectbox('課程 / Program', options, format_func=lambda x: dic[x])
+st.sidebar.multiselect('年次 / Year', [1,2,3,4])
+st.sidebar.multiselect('学期 / Semester', ['前期','後期','第1ｸｫｰﾀ','第2ｸｫｰﾀ','通年'])
 
 # About
-st.title('Course Syllabus App')
-head = st.beta_expander('About')
-head.markdown('''
+st.write('''# Web Syllabus App
 ### Kyoto Institute of Technology (KIT)
-This will show all the results for each faculty you selected.
-- **Python libraries: ** streamlit, pandas
-- **Data source: ** [syllabus.kit.ac.jp](https://www.syllabus.kit.ac.jp/)
 ''')
-
-where = st.sidebar.selectbox('課程 / Program ', [url1,url2,url3,url4]) 
-st.sidebar.multiselect('年次 / Year', [1,2,3,4])
-st.sidebar.multiselect('学期 / Semester', ['前期','後期'])
 
 @st.cache
 def main(URL):  # sourcery no-metrics
@@ -34,22 +44,19 @@ def main(URL):  # sourcery no-metrics
     year_list=[]
     sem_list=[]
 
-    pages_to_find = 4
+    pages_to_find = 20
     pages =[]
     for i in range(1,pages_to_find+1):
         urls = f'{where}{i}' 
         pages.append(urls)
 
-    # print(pages)
-
     for url in pages:
         try:
             response = r.get(url).text
             soup = BeautifulSoup(response,'lxml')
-            print(r.get(url))
+            print(r.get(url)) # check if the response is OK [200]
         except:
             print("Page cannot be found. Try again.")
-
 
         # COURSE TITLE
         try:
@@ -96,7 +103,6 @@ def main(URL):  # sourcery no-metrics
             pass
 
         ## SEMESTER
-
         try:
             for y in soup.find('table',class_='gen_tbl2 data_list_tbl').find_all('tr'):
                 if y != None:
@@ -116,16 +122,19 @@ def main(URL):  # sourcery no-metrics
             'Year' : year_list,
             'Semester' : sem_list
         }
-    print('success')
+    print('Retrieving data was successful !')
 
-    ## Create dataframe from the dictionary\
+    ## Create dataframe from the dictionary
     df = pd.DataFrame(data=data)
     df.index+=1
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', None)
-    pd.set_option('display.max_colwidth', -1)
     return df
 
-df  = main(where)
+df  = main(url)
 st.write(df)
+
+head = st.beta_expander('About')
+head.markdown('''
+An app that will show all the results for each faculty you selected. (in-progress)
+- **Python libraries: ** streamlit, pandas
+- **Data source: ** [syllabus.kit.ac.jp](https://www.syllabus.kit.ac.jp/?c=menu&sk=00)
+''')
